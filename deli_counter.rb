51d9d91b0c5d64b0284b03276c92d7e1998a40e4
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-07 21:56:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ def line(array)
 end
 
 def take_a_number(array, name)
-  puts "Welcome, #{name}. You are number #{array.size+1} in line."
+  puts "Welcome, #{name}. You are number #{array.size} in line."
 end
