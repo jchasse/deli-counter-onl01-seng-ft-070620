ba665cc0b30ea6fa9a8051fea59bc79574155a46
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-07 21:35:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
     puts "The line is currently empty."
   else
     queue = array.each_with_index do |name, index|
-      puts " #{index+1}. #{name}"
+      " #{index+1}. #{name}"
     end
     puts "The line is currently: #{queue}"
   end
