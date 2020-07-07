a6bf329a5765680e3cc162ec43b14c6f1a6827b2
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-07 21:28:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+
+katz_deli = []
+
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    queue = array.each_with_index do |name, index|
+      puts " #{index+1}. #{name}"
+    end
+    puts "The line is currently: #{queue}"
+  end
+end
